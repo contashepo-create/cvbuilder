7ba73377bc592f1b106fead8738b9fb8862c973f
contashepo-create <conta.shepo@gmail.com>
2026-06-30 22:44:50 +0300

    Fix: set base path to /cvbuilder/ for GitHub Pages + add Router basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,14 @@ import App from './App.jsx'
 import './i18n/index.js'
 import './index.css'
 
+// Detect GitHub Pages subpath basename
+const basename = window.location.hostname.includes('github.io')
+  ? '/' + window.location.pathname.split('/')[1] + '/'
+  : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
